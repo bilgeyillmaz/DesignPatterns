e7f00d5071bb785b343da31ad6e035b6bbe170da
bilgeyillmaz <96742976+bilgeyillmaz@users.noreply.github.com>
2022-03-21 11:45:15 +0300

    Abstract Factory Design

---
+++ AbstractFactoryDesign/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactoryDesign
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new Factory1());
+            productManager.GetAll();
+            ProductManager productManager2 = new ProductManager(new Factory2());
+            productManager2.GetAll();   
+            Console.ReadKey();  
+        }
+    }
+    public abstract class Logging
+    {
+        public abstract void Log(string message);   
+    }
+    public class Log4NetLogger : Logging
+    {
+        public override void Log(string message)
+        {
+            Console.WriteLine("Logged with Log4Net!");
+        }
+    }
+    public class NLogger : Logging
+    {
+        public override void Log(string message)
+        {
+            Console.WriteLine("Logged with NLogger!");
+        }
+    }
+    public abstract class Caching
+    {
+        public abstract void Cache(string message);
+    }
+    public class MemCache : Caching
+    {
+        public override void Cache(string message)
+        {
+            Console.WriteLine("Cached with MemCache");
+        }
+    }
+    public class RedisCache : Caching
+    {
+        public override void Cache(string message)
+        {
+            Console.WriteLine("Cached with RedisCache");
+        }
+    }
+    public abstract class CrossCuttingConcernsFactory// bu fabrikadan yeni fabrikalar üretebiliriz.
+    {
+        public abstract Logging CreateLogger();
+        public abstract Caching CreateCaching();
+    }
+    public class Factory1 : CrossCuttingConcernsFactory
+    {
+        public override Caching CreateCaching()
+        {
+            return new MemCache(); 
+        }
+
+        public override Logging CreateLogger()
+        {
+            return new Log4NetLogger();  
+        }
+    }
+    public class Factory2 : CrossCuttingConcernsFactory
+    {
+        public override Caching CreateCaching()
+        {
+            return new RedisCache();
+        }
+
+        public override Logging CreateLogger()
+        {
+            return new NLogger();
+        }
+    }
+    public class ProductManager
+    {
+        private CrossCuttingConcernsFactory _crossCuttingConcernsFactory;
+        private Logging _logging;
+        private Caching _caching;
+
+        public ProductManager(CrossCuttingConcernsFactory crossCuttingConcernsFactory)
+        {
+            _crossCuttingConcernsFactory = crossCuttingConcernsFactory;
+            _logging = _crossCuttingConcernsFactory.CreateLogger();
+            _caching= _crossCuttingConcernsFactory.CreateCaching();  
+        }
+
+        public void GetAll()
+        {
+            _logging.Log("Logged");
+            _caching.Cache("Data");
+            Console.WriteLine("Products listed!");
+        }
+    }
+}
